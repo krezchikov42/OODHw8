e9ede7388f99d5f16bc1c5237c99883ee0956add
Will Parker <wparker1106@gmail.com>
2017-12-03 20:38:29 -0500

    Added tests

---
+++ src/cs3500/our_animator/controller/ProviderControllerTest.java
@@ -0,0 +1,58 @@
+package cs3500.our_animator.controller;
+
+import static org.junit.Assert.*;
+
+import cs3500.animator.view.SimpleAnimationView;
+import cs3500.animator.view.visual.InteractiveSwingView;
+import cs3500.animator.view.visual.SwingView;
+import cs3500.our_animator.Action;
+import cs3500.our_animator.Color;
+import cs3500.our_animator.EasyShape.PinHole;
+import cs3500.our_animator.MoveAction;
+import cs3500.our_animator.Point;
+import cs3500.our_animator.Rectangle;
+import cs3500.our_animator.model.EasyAnimatorModel;
+import cs3500.our_animator.model.EasyAnimatorOperations;
+import org.junit.Test;
+
+public class ProviderControllerTest {
+
+  @Test
+  public void testRunHybrid() {
+    EasyAnimatorOperations m = new EasyAnimatorModel();
+
+    Rectangle r = new Rectangle(20, 20, new Point(100, 100), PinHole.Top,
+        "rectangle", 0, 100, new Color(0.5, 0.5, 0.5));
+
+    m.addShape(r);
+
+    Action a = new MoveAction(r, new Point(100,100), new Point(300, 300), 0, 10);
+
+    SimpleAnimationView v = new InteractiveSwingView();
+
+    Controller c = new ProviderController(m, v, 1);
+
+    c.runViewWithVisualComponent();
+  }
+
+  @Test
+  public void testRunVisual() {
+    EasyAnimatorOperations m = new EasyAnimatorModel();
+
+    Rectangle r = new Rectangle(20, 20, new Point(100, 100), PinHole.Top,
+        "rectangle", 0, 100, new Color(0.5, 0.5, 0.5));
+
+    Action a = new MoveAction(r, new Point(100,100), new Point(300, 300), 0, 10);
+    r.addAction(a);
+
+    m.addShape(r);
+
+
+
+    SimpleAnimationView v = new SwingView();
+
+    Controller c = new ProviderController(m, v, 10);
+
+    c.runViewWithVisualComponent();
+  }
+}
